79b8e161e9c464cb2386660ca28f9662cd737d63
Parissa Nahachewsky <parissa.nahachewsky@cra-arc.gc.ca>
2026-04-24 09:22:32 -0700

    Check out output and update templates and d2gc script

--- source/data/d2gc-scripts/D2GC/d2gc.ps1
+++ source/data/d2gc-scripts/D2GC/d2gc.ps1
@@ -744,7 +744,8 @@ function setWrapper ($array, $json) {
     $text = replaceSquareBrackets $text
     foreach ($attr in $array.attributes) {
         if ( $attr.Name -ne "required" -and $attr.Name -ne "split" -and $attr.Name -ne "source" -and $attr.Name -ne "sub" -and $attr.Name -ne "header" -and $attr.Name -ne "for" -and $attr.Name -ne "break" -and $attr.Name -ne "hash" -and $attr.Name -ne "sort" -and $attr.Name -ne "includes" -and $attr.Name -ne "exclude") {
-            $attrib += ($attr.Name) + "=""" + ($attr."#text") + """ "
+            $attrText = [System.Net.WebUtility]::HtmlEncode($attr."#text")
+            $attrib += ($attr.Name) + "=""" + $attrText + """ "
         }
     }
 
